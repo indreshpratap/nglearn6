a15869440c4a47867416061bdfca00a4b077099d
Indresh <ips.indresh@gmail.com>
2018-03-24 12:09:17 +0530

    lifecycle hooks

--- src/app/products/pro-detail/pro-detail.component.ts
+++ src/app/products/pro-detail/pro-detail.component.ts
@@ -1,10 +1,12 @@
-import { Component, OnInit, Input, EventEmitter, Output } from "@angular/core";
+import { Component, OnInit, Input, EventEmitter, Output, OnDestroy, AfterViewInit, AfterViewChecked, DoCheck } from "@angular/core";
 
 @Component({
   selector: "app-pro-detail",
   templateUrl: "pro-detail.component.html"
 })
-export class ProDetailComponent implements OnInit {
+export class ProDetailComponent implements OnInit,OnDestroy,AfterViewInit,AfterViewChecked,DoCheck {
+ 
+ 
   @Input() product: any;
   @Input("indx") index:number; // indx is an alias for index 
   @Output() onAddToCart: EventEmitter<any> = new EventEmitter<any>();
@@ -12,6 +14,21 @@ export class ProDetailComponent implements OnInit {
   ngOnInit(){
     console.log("Product Details NgOnInit",this.index);
   }
+  ngOnDestroy(): void {
+    console.log("Product Details ngonDestroy",this.index);
+  }
+  ngAfterViewInit(): void {
+    console.log("product details ngAfterViewInit",this.index);
+  }
+
+   
+  ngAfterViewChecked(): void {
+    console.log("product details After view checked",this.index);
+  }
+  ngDoCheck(): void {
+   console.log("Ngdocheck",this.index);
+  }
+  
   cartButtonClick() {
     let data = { productId: this.product.id, productTitle: this.product.title };
     console.log("Sending data=", data);
--- src/app/products/pro-listing/pro.listing.component.ts
+++ src/app/products/pro-listing/pro.listing.component.ts
@@ -3,16 +3,16 @@ import { Component, OnInit } from "@angular/core";
 @Component({
   selector: "app-pro-listing",
   templateUrl: "pro.listing.component.html",
-  styles:['.first,.last {font-weight:bold}']
+  styles: [".first,.last {font-weight:bold}"]
 })
 export class ProListingComponent implements OnInit {
   productList: Array<any>;
   gridDisplay = true;
   cartList = [];
 
-ngOnInit(){
-console.log("Product lising ngOnInit");
-}
+  ngOnInit() {
+    console.log("Product lising ngOnInit");
+  }
   constructor() {
     console.log("Product listing constructor");
     this.productList = [
@@ -81,55 +81,52 @@ console.log("Product lising ngOnInit");
         product_type: "Shirts",
         tags: ["mensware", "tshirts"]
       }
-      
-    
     ];
   }
 
-  
-  pushToTop(){
-      this.productList.unshift({
-        id: 1000,
-        title: "Shopify T-Samsung",
-        handle: "shopify-mobile",
-        body_html:
-          '\u003cp\u003eSo this is a product.\u003c/p\u003e\n\n\n\t\u003cp\u003eThe text you see here is a Product Description. Every product has a price, a weight, a picture and a description.  To edit the description of this product or to create a new product you can go to the \u003ca href="/admin/products"\u003eProducts Tab\u003c/a\u003e of the administration menu.\u003c/p\u003e\n\n\n\t\u003cp\u003eOnce you have mastered the creation and editing of products you will want your products to show up on your Shopify site.  There is a two step process to do this.\u003c/p\u003e\n\n\n\t\u003cp\u003eFirst you need to add your products to a Collection.  A Collection is an easy way to group products together. If you go to the \u003ca href="/admin/collections"\u003eCollections Tab\u003c/a\u003e of the administration menu you can begin creating collections and adding products to them.\u003c/p\u003e\n\n\n\t\u003cp\u003eSecond you\u0026#8217;ll need to create a link from your shop\u0026#8217;s navigation menu to your Collections. You can do this by going to the \u003ca href="/admin/links"\u003eNavigations Tab\u003c/a\u003e of the administration menu and clicking on \u0026#8220;Add a link\u0026#8221;.\u003c/p\u003e\n\n\n\t\u003cp\u003eGood luck with your shop!\u003c/p\u003e',
-        published_at: "2006-06-03T01:28:13-04:00",
-        created_at: "2006-06-03T01:28:13-04:00",
-        updated_at: "2014-08-05T16:58:27-04:00",
-        vendor: "Shopify",
-        product_type: "Mobile",
-        tags: ["mobile", "android"]
-      });
+  pushToTop() {
+    this.productList.unshift({
+      id: 1000,
+      title: "Shopify T-Samsung",
+      handle: "shopify-mobile",
+      body_html:
+        '\u003cp\u003eSo this is a product.\u003c/p\u003e\n\n\n\t\u003cp\u003eThe text you see here is a Product Description. Every product has a price, a weight, a picture and a description.  To edit the description of this product or to create a new product you can go to the \u003ca href="/admin/products"\u003eProducts Tab\u003c/a\u003e of the administration menu.\u003c/p\u003e\n\n\n\t\u003cp\u003eOnce you have mastered the creation and editing of products you will want your products to show up on your Shopify site.  There is a two step process to do this.\u003c/p\u003e\n\n\n\t\u003cp\u003eFirst you need to add your products to a Collection.  A Collection is an easy way to group products together. If you go to the \u003ca href="/admin/collections"\u003eCollections Tab\u003c/a\u003e of the administration menu you can begin creating collections and adding products to them.\u003c/p\u003e\n\n\n\t\u003cp\u003eSecond you\u0026#8217;ll need to create a link from your shop\u0026#8217;s navigation menu to your Collections. You can do this by going to the \u003ca href="/admin/links"\u003eNavigations Tab\u003c/a\u003e of the administration menu and clicking on \u0026#8220;Add a link\u0026#8221;.\u003c/p\u003e\n\n\n\t\u003cp\u003eGood luck with your shop!\u003c/p\u003e',
+      published_at: "2006-06-03T01:28:13-04:00",
+      created_at: "2006-06-03T01:28:13-04:00",
+      updated_at: "2014-08-05T16:58:27-04:00",
+      vendor: "Shopify",
+      product_type: "Mobile",
+      tags: ["mobile", "android"]
+    });
   }
 
-  addLast(){
-      this.productList.push({
-        id: 2000,
-        title: "Shopify T-MI-TV",
-        handle: "shopify-mi-tv",
-        body_html:
-          '\u003cp\u003eSo this is a product.\u003c/p\u003e\n\n\n\t\u003cp\u003eThe text you see here is a Product Description. Every product has a price, a weight, a picture and a description.  To edit the description of this product or to create a new product you can go to the \u003ca href="/admin/products"\u003eProducts Tab\u003c/a\u003e of the administration menu.\u003c/p\u003e\n\n\n\t\u003cp\u003eOnce you have mastered the creation and editing of products you will want your products to show up on your Shopify site.  There is a two step process to do this.\u003c/p\u003e\n\n\n\t\u003cp\u003eFirst you need to add your products to a Collection.  A Collection is an easy way to group products together. If you go to the \u003ca href="/admin/collections"\u003eCollections Tab\u003c/a\u003e of the administration menu you can begin creating collections and adding products to them.\u003c/p\u003e\n\n\n\t\u003cp\u003eSecond you\u0026#8217;ll need to create a link from your shop\u0026#8217;s navigation menu to your Collections. You can do this by going to the \u003ca href="/admin/links"\u003eNavigations Tab\u003c/a\u003e of the administration menu and clicking on \u0026#8220;Add a link\u0026#8221;.\u003c/p\u003e\n\n\n\t\u003cp\u003eGood luck with your shop!\u003c/p\u003e',
-        published_at: "2006-06-03T01:28:13-04:00",
-        created_at: "2006-06-03T01:28:13-04:00",
-        updated_at: "2014-08-05T16:58:27-04:00",
-        vendor: "Shopify",
-        product_type: "Television",
-        tags: ["MI", "TV"]
-      });
+  addLast() {
+    this.productList.push({
+      id: 2000,
+      title: "Shopify T-MI-TV",
+      handle: "shopify-mi-tv",
+      body_html:
+        '\u003cp\u003eSo this is a product.\u003c/p\u003e\n\n\n\t\u003cp\u003eThe text you see here is a Product Description. Every product has a price, a weight, a picture and a description.  To edit the description of this product or to create a new product you can go to the \u003ca href="/admin/products"\u003eProducts Tab\u003c/a\u003e of the administration menu.\u003c/p\u003e\n\n\n\t\u003cp\u003eOnce you have mastered the creation and editing of products you will want your products to show up on your Shopify site.  There is a two step process to do this.\u003c/p\u003e\n\n\n\t\u003cp\u003eFirst you need to add your products to a Collection.  A Collection is an easy way to group products together. If you go to the \u003ca href="/admin/collections"\u003eCollections Tab\u003c/a\u003e of the administration menu you can begin creating collections and adding products to them.\u003c/p\u003e\n\n\n\t\u003cp\u003eSecond you\u0026#8217;ll need to create a link from your shop\u0026#8217;s navigation menu to your Collections. You can do this by going to the \u003ca href="/admin/links"\u003eNavigations Tab\u003c/a\u003e of the administration menu and clicking on \u0026#8220;Add a link\u0026#8221;.\u003c/p\u003e\n\n\n\t\u003cp\u003eGood luck with your shop!\u003c/p\u003e',
+      published_at: "2006-06-03T01:28:13-04:00",
+      created_at: "2006-06-03T01:28:13-04:00",
+      updated_at: "2014-08-05T16:58:27-04:00",
+      vendor: "Shopify",
+      product_type: "Television",
+      tags: ["MI", "TV"]
+    });
   }
   toogleMode() {
     this.gridDisplay = !this.gridDisplay;
   }
-  pushToCart(event){
-    console.log('Received at parent end and data is: ', event);
-    event.qty=1;
+  pushToCart(event) {
+    console.log("Received at parent end and data is: ", event);
+    event.qty = 1;
     this.cartList.push(event);
   }
 
-  removeFromCart(indx){
-    if(confirm("Are you sure want to remove?")){
-      this.cartList.splice(indx,1);
+  removeFromCart(indx) {
+    if (confirm("Are you sure want to remove?")) {
+      this.cartList.splice(indx, 1);
     }
   }
 
